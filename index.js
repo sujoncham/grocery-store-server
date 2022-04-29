d520209bd24e79cb0c098ac296d806fc8563742b
sujon-chambugong <sujonchambugong@gmail.com>
2022-04-30 04:06:25 +0600

    node server testing

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const expess = require('express');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.send("Grocery ware house running");
+})
+
+app.listen(port, ()=>{
+    console.log("My port is", port);
+})
