665b3f5d7d6008b524ff0eb45992402660fe71c9
sujon-chambugong <sujonchambugong@gmail.com>
2022-04-30 18:35:12 +0600

    product post successfully

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 // middleware 
@@ -15,15 +16,46 @@ app.get('/', (req, res)=>{
 })
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://grocerywareHouse:<password>@cluster0.zxfur.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zxfur.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log("mongodb connected")
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+    try{
+        await client.connect();
+        const stockCollection = client.db("stockManagement").collection("grocery");
+    
+            // get data from server 
+        app.get('/inventory', async (req, res)=>{
+            const query = {};
+            const cursor = stockCollection.find(query);
+            const stock = await cursor.toArray();
+            res.send(stock);
+        });
+
+            // showing product by id 
+        app.get('/inventory/:id', async (req, res)=>{
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)};
+            const stock = await stockCollection.findOne(query);
+            res.send(stock);
+        })
+
+        // stock post 
+        app.post('/inventory', async (req, res)=>{
+            const newStock = req.body;
+            const result = await stockCollection.insertOne(newStock);
+            res.send(result);
+        })
+
+
+
+    } 
+    finally{
+
+    }
+} 
+run().catch(console.dir);
 
 
 app.listen(port, ()=>{
